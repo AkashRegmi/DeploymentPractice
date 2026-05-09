b2b7496315c54c7c5f3d83c4efe3affc3c06915c
Akash Regmi <akashregmi82@gmail.com>
2026-05-09 17:54:15 +0545

    fix thecors error

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 //this is tyhe middlewaere
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://deploymentpractice-1-u6z0.onrender.com",
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
